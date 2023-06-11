119e4cd29374b9dc0a3cf97d512229986259b0ba
smk-dev20 <smk.dev20@gmail.com>
2023-06-11 14:50:49 -0700

    ec2,ebs

---
+++ instance.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "tf-example" {
+    ami = var.AMIS[var.AWS_REGION]
+    instance_type = "t2.micro"
+
+    subnet_id = aws_subnet.tf-main-public-subnet-1.id
+
+    vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+    key_name = aws_key_pair.mykeypair.key_name
+}
+
+resource "aws_ebs_volume" "ebs-volume-1" {
+    availability_zone = "us-east-1a"
+    size = 20
+    type = "gp2"
+    tags = {
+        Name = "extra volume data"
+    }
+}
+
+resource "aws_volume_attachment" "ebs-volume-1-attachment" {
+    device_name = "/dev/xvdh"
+    volume_id = aws_ebs_volume.ebs-volume-1.id
+    instance_id = aws_instance.tf-example.id
+    stop_instance_before_detaching = true
+}
--- vars.tf
+++ vars.tf
@@ -9,3 +9,18 @@ variable AWS_ACCESS_KEY {
 variable AWS_SECRET_KEY {
     
 }
+
+variable PATH_TO_PRIVATE_KEY {
+
+}
+
+variable PATH_TO_PUBLIC_KEY {
+
+}
+
+variable AMIS {
+    type = map(string)
+    default = {
+        us-east-1 = "ami-053b0d53c279acc90"
+    }
+}
